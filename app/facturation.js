998c6c47d6b52445fac11028412cb59a0f1f83a2
Cavallucci <laura.cllucci@gmail.com>
2023-07-31 21:25:04 +0200

    ✍ adding adhesions

---
+++ app/facturation.js
@@ -0,0 +1,49 @@
+function fillCustomersList(groupedData) {
+    const t_customers = [];
+
+    for (let i = 0; i < groupedData.length; i++) {
+        const customerData = groupedData[i];
+        let existingCustomer = t_customers.find((t_customer) => t_customer.customerId === customerData[4]);
+
+        if (existingCustomer) {
+        existingCustomer.courses.push(customerData[8]);
+        existingCustomer.totalRestantDu += customerData[3];
+        } else {
+        const newCustomer = {
+            customerId: customerData[4],
+            customerFirstName: customerData[5],
+            totalRestantDu: customerData[3],
+            courses: [customerData[8]]
+        };
+        t_customers.push(newCustomer);
+        }
+    }
+    return t_customers;
+}
+  
+function displayFacturation(groupedData) {
+    const container = document.getElementById('displayContainer');
+    container.innerHTML = ''; // Reset the content of the container
+
+    const t_customers = fillCustomersList(groupedData);
+
+    for (let i = 0; i < t_customers.length; i++) {
+        const customerInfo = document.createElement('div');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `customerCheckbox_${t_customers[i].customerId}`; // Unique ID for each checkbox
+        customerInfo.appendChild(checkbox);
+
+        const label = document.createElement('label');
+        label.textContent = `Le customer ${t_customers[i].customerFirstName} numéro ${t_customers[i].customerId} a un total de ${t_customers[i].totalRestantDu}€ restant du pour les cours suivants : ${t_customers[i].courses}`;
+        customerInfo.appendChild(label);
+
+        container.appendChild(customerInfo);
+    }
+}
+
+module.exports = {
+    displayFacturation,
+    fillCustomersList
+  };
--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,7 @@
 const { ipcRenderer } = require('electron');
+const facturationModule = require('./facturation');
+const adhesionModule = require('./adhesion');
+
 
 document.getElementById('fileInput').addEventListener('change', () => {
   console.log('Clic sur le bouton "Trier"');
@@ -18,7 +21,19 @@ document.getElementById('printButton').addEventListener('click', () => {
 
 ipcRenderer.on('sortingSuccess', (event, groupedData) => {
   if (groupedData.length > 0) {
-    displayGroupedData(groupedData);
+    const radioGroup = document.getElementsByName("options");
+    radioGroup.forEach((radioButton) => {
+      radioButton.addEventListener("change", () => {
+        if (radioButton.checked) {
+          if (radioButton.id === 'facturation') {
+            facturationModule.displayFacturation(groupedData);
+          }
+          else if (radioButton.id === 'adhesion') {
+            adhesionModule.displayAdhesion(groupedData);
+          }
+        }
+      });
+    });
   }
   else {
     alert('Aucune donnée détecté');
@@ -40,48 +55,3 @@ ipcRenderer.on('printError', (event, error) => {
   alert(error);
   console.error(error);
 });
-
-function fillCustomersList(groupedData) {
-  const t_customers = [];
-
-  for (let i = 0; i < groupedData.length; i++) {
-    const customerData = groupedData[i];
-    let existingCustomer = t_customers.find((t_customer) => t_customer.customerId === customerData[4]);
-  
-    if (existingCustomer) {
-      existingCustomer.courses.push(customerData[8]);
-      existingCustomer.totalRestantDu += customerData[3];
-    } else {
-      const newCustomer = {
-        customerId: customerData[4],
-        customerFirstName: customerData[5],
-        totalRestantDu: customerData[3],
-        courses: [customerData[8]]
-      };
-      t_customers.push(newCustomer);
-    }
-  }
-  return t_customers;
-}
-
-function displayGroupedData(groupedData) {
-  const container = document.getElementById('displayContainer');
-  container.innerHTML = ''; // Reset the content of the container
-
-  const t_customers = fillCustomersList(groupedData);
-
-  for (let i = 0; i < t_customers.length; i++) {
-    const customerInfo = document.createElement('div');
-
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = `customerCheckbox_${t_customers[i].customerId}`; // Unique ID for each checkbox
-    customerInfo.appendChild(checkbox);
-
-    const label = document.createElement('label');
-    label.textContent = `Le customer ${t_customers[i].customerFirstName} numéro ${t_customers[i].customerId} a un total de ${t_customers[i].totalRestantDu}€ restant du pour les cours suivants : ${t_customers[i].courses}`;
-    customerInfo.appendChild(label);
-
-    container.appendChild(customerInfo);
-  }
-}
